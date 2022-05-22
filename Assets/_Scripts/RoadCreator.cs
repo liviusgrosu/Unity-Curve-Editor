578adae31b26f95a463f270f5767771e80b5cccc
liviusgrosu <livius.grosu@ryerson.ca>
2022-05-22 19:10:45 -0400

    Added uv coordinates to path mesh to allow the application of a texture

--- Assets/_Scripts/RoadCreator.cs
+++ Assets/_Scripts/RoadCreator.cs
@@ -16,15 +16,17 @@ public class RoadCreator : MonoBehaviour
     {
         Path path = GetComponent<PathCreator>().path;
         Vector2[] points = path.CalculateEvenlySpacedPoints(Spacing);
-        GetComponent<MeshFilter>().mesh = CreateRoadMesh(points);
+        GetComponent<MeshFilter>().mesh = CreateRoadMesh(points, path.IsClosed);
     }
 
-    private Mesh CreateRoadMesh(Vector2[] points)
+    private Mesh CreateRoadMesh(Vector2[] points, bool isClosed)
     {
         // 2n
         Vector3[] vertices = new Vector3[points.Length * 2];
+        Vector2[] uvs = new Vector2[points.Length * 2];
         // 2(n-1)
-        int[] triangles = new int[2 * (points.Length - 1) * 3];
+        int numberOfTriangles = 2 * (points.Length - 1) + (isClosed ? 2 : 0);
+        int[] triangles = new int[numberOfTriangles * 3];
         int vertexIndex = 0;
         int triangleIndex = 0;
 
@@ -32,14 +34,14 @@ public class RoadCreator : MonoBehaviour
         {
             Vector2 forward = Vector2.zero;
             // Not the last point
-            if (i < points.Length - 1)
+            if (i < points.Length - 1 || isClosed)
             {
-                forward += points[i + 1] - points[i];
+                forward += points[(i + 1) % points.Length] - points[i];
             }
             // Not the first point
-            if (i > 0)
+            if (i > 0 || isClosed)
             {
-                forward += points[i] - points[i - 1];
+                forward += points[i] - points[(i - 1 + points.Length) % points.Length];
             }
 
             // take average of first and second conditions. Not first or last point
@@ -51,17 +53,23 @@ public class RoadCreator : MonoBehaviour
             vertices[vertexIndex] = points[i] + left * RoadWidth * 0.5f;
             vertices[vertexIndex + 1] = points[i] - left * RoadWidth * 0.5f;
 
-            if (i < points.Length - 1)
+            // Add each UV coordinate based off how complete the path is
+            float completionPercent = i / (float)(points.Length - 1);
+            float v = 1 - Mathf.Abs(2 * completionPercent - 1);
+            uvs[vertexIndex] = new Vector2(0, v);
+            uvs[vertexIndex + 1] = new Vector2(1, v);
+
+            if (i < points.Length - 1 || isClosed)
             {
                 // First triangle
                 triangles[triangleIndex] = vertexIndex;
-                triangles[triangleIndex + 1] = vertexIndex + 2;
+                triangles[triangleIndex + 1] = (vertexIndex + 2) % vertices.Length;
                 triangles[triangleIndex + 2] = vertexIndex + 1;
 
                 // Second triangle
                 triangles[triangleIndex + 3] = vertexIndex + 1;
-                triangles[triangleIndex + 4] = vertexIndex + 2;
-                triangles[triangleIndex + 5] = vertexIndex + 3;
+                triangles[triangleIndex + 4] = (vertexIndex + 2) % vertices.Length;
+                triangles[triangleIndex + 5] = (vertexIndex + 3) % vertices.Length;
             }
 
             vertexIndex += 2;
@@ -71,6 +79,7 @@ public class RoadCreator : MonoBehaviour
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uvs;
         return mesh;
     }
 }
